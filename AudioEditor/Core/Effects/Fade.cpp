afabfe6acfb6329645525d6c5cc696f0d8433c5d
Anna Galstyan <anulgal04@gmail.com>
2025-10-25 17:55:45 +0400

    removed unnecessary core audio engine and adapter files

--- AudioEditor/Core/Effects/Fade.cpp
+++
@@ -1,6 +0,0 @@
-#include "Fade.h"
-#include "../Logger.h"
-
-void Fade::apply(float* audioBuffer, size_t bufferSize) {
-    Logger::getInstance().log("Applying Fade Effect");
-}
--- AudioEditor/Core/Effects/Fade.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "IEffect.h"
-
-class Fade : public IEffect {
-public:
-    void apply(float* audioBuffer, size_t bufferSize) override;
-};
